d2ca965b41dabdbf0a0d19ebb8eff8e5cf089c94
Sayan Lodh <33943721+SkullCrusherx@users.noreply.github.com>
2025-06-20 01:12:43 +0530

    Update file organiser.py

--- file organiser.py
+++ file organiser.py
@@ -1,4 +1,4 @@
-from tkinter import Tk,Label,Button,filedialog,Entry
+from tkinter import Tk,Label,Button,filedialog,Entry,Frame
 import os
 import shutil
 
@@ -12,6 +12,9 @@ lvl = Label(root,text='Welcome to File Orgsniser',background='#717fad',font=("ar
 lvl.pack()
 root.config(background='#717fad')
 
+frame1 = Frame(root,background='#717fad')
+frame1.pack()
+
 
 def openFile(): # Function
     global filepath
@@ -19,11 +22,12 @@ def openFile(): # Function
     global files
     global lvl2
     files = os.listdir(filepath)
-    lvl1 = Entry(root,text = filepath,background='#717fad')
-    lvl1.place(x=100,y=70)
-    
+    lvl1 = Entry(root,background='#717fad',width=35)
+    lvl1.insert(0, filepath)
+    lvl1.place(x=25,y=70)
+
     lvl2 = Label(root,text = "Ready To Start!",background='#717fad')
-    lvl2.place(x=100,y=130)
+    lvl2.place(x=90,y=135)
 
 def startmanaging():
     for file in files:
@@ -38,11 +42,11 @@ def startmanaging():
             os.makedirs(filepath + '/' + extension)
             shutil.move(filepath + '/' + file, filepath + '/' + extension + '/' + file)
 #button type 1
-btn1 = Button(root,text="Browse",background='#cad5fa',command=openFile)
+btn1 = Button(root,text="Browse",background='#cad5fa',command=openFile,font=("arial",10,"bold"))
 btn1.place(x=100,y=40)
 
 #button type 2
-btn = Button(root,text="start",background='#cad5fa',command = startmanaging)
+btn = Button(root,text="START",background='#cad5fa',command = startmanaging,font=("arial",10,"bold"),width=7)
 btn.place(x=100,y=100)
 
 root.mainloop()
